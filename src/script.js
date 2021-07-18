75a8e62232a8bd127159d68b01cefe12f6d0f39d
Rhavyx <everluca@outlook.com>
2021-07-18 17:24:48 -0300

    :sparkles: texture & particles simple animation

--- src/script.js
+++ src/script.js
@@ -17,7 +17,7 @@ const scene = new THREE.Scene()
 TEXTURES - FREE Particles texture https://www.kenney.nl/assets/particle-pack
 ==================================================================================*/
 const textureLoader = new THREE.TextureLoader()
-
+const particleTexture = textureLoader.load('/textures/particles/magic_02.png')
 
 /*==================================================================================
 PARTICLES
@@ -25,9 +25,11 @@ PARTICLES
 const particlesGeometry =  new THREE.BufferGeometry();
 const particlePositions = new Float32Array(1000 * 3)
 const numberOfStars = 5000;
+const colors = new Float32Array(numberOfStars * 3);
 
-for ( let i = 0; i < numberOfStars; i ++ ) {
-	particlePositions[i] = (Math.random() - 0.5) * 10
+for ( let i = 0; i < numberOfStars * 3; i ++ ) {
+	particlePositions[i] = (Math.random() - 0.5) * 20
+    colors[i] = Math.random()
 }
 
 particlesGeometry.setAttribute( 
@@ -35,11 +37,25 @@ particlesGeometry.setAttribute(
     new THREE.Float32BufferAttribute( particlePositions, 3 ) 
 );
 
+particlesGeometry.setAttribute( 
+    'color', 
+    new THREE.BufferAttribute( colors, 3 ) 
+);
+
 const particlesMaterial = new THREE.PointsMaterial( {
-    size: 0.01, 
+    size: 0.1, 
     sizeAttenuation: true,
-    color: 0x888888 
+    //color: 0x888888 
 } );
+//particlesMaterial.map = particleTexture;
+particlesMaterial.transparent = true
+particlesMaterial.alphaMap = particleTexture;
+//particlesMaterial.alphaTest = 0.1
+//particlesMaterial.depthTest = false
+particlesMaterial.depthWrite = false
+particlesMaterial.blending = THREE.AdditiveBlending
+
+particlesMaterial.vertexColors = true
 
 const particlesPoints = new THREE.Points( particlesGeometry, particlesMaterial );
 
@@ -93,6 +109,15 @@ const clock = new THREE.Clock()
 const frameUpdate = () =>
 {
     const elapsedTime = clock.getElapsedTime()
+
+    for(let i = 0; i < numberOfStars; i++){
+        const i3 = i * 3;
+        const x = particlesGeometry.attributes.position.array[i3 + 0];
+        particlesGeometry.attributes.position.array[i3 + 1] = Math.sin(elapsedTime + x)
+    }
+    particlesGeometry.attributes.position.needsUpdate = true
+    //Updating particles
+    //particlesPoints.rotation.y = elapsedTime * 0.03;
 
     controls.update()
     renderer.render(scene, camera)
